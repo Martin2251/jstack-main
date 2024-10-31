5983136ac3466ebc13e46a0201c2f05d482efc97
Martin Lowe <martinlowe673@gmail.com>
2024-10-31 20:01:18 +0000

    wrapper

--- src/app/components/max-width-wrapper.tsx
+++ src/app/components/max-width-wrapper.tsx
@@ -8,6 +8,6 @@ interface MaxWidthWrapperProps {
 
 export const MaxWidthWrapper = ({className, children}:MaxWidthWrapperProps) =>{
     return (
-        <div className={cn()}></div>
+        <div className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20")}></div>
     )
 }
