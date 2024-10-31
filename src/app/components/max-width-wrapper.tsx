cb4aec08bb80180b2be35e33173fca4ddf0e890c
Martin Lowe <martinlowe673@gmail.com>
2024-10-31 19:58:41 +0000

    interface

--- src/app/components/max-width-wrapper.tsx
+++ src/app/components/max-width-wrapper.tsx
@@ -1,3 +1,9 @@
+interface MaxWidthWrapperProps {
+    children: React.ReactNode;
+    className?:string,
+}
+
+
 export const MaxWidthWrapper = ({className, children}:MaxWidthWrapperProps) =>{
     return (
         <div></div>
